d616b96690dde91bf6c052c1e059f66d85be3f58
Jared Massey <jared@jaredmassey.com>
2021-01-09 16:37:13 -0700

    Basic decorator, begin building UI components using tk grid, function to load target functions from module / class instance

--- pygenerategui.py
+++ pygenerategui.py
@@ -21,39 +21,114 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import tkinter as tk
-from robot.api.deco import keyword
+from tkinter import *
+from tkinter import ttk
+import inspect
+from typing import Union, get_args, get_origin
 
 
-class Application(tk.Frame):
+def pggui(name=None):
+    if callable(name):
+        return pggui()(name)
+    def decorator(func):
+        func._pggui_name = name if name is not None else func.__name__
+        return func
+    return decorator
+
+
+def load_funcs(component):
+    """
+    Modeled off of robotlibcore's add_library_components
+    :param component: A module or an instance of a class, containing functions or methods to be potentially tuned into GUIs
+    :return: A list containing any functions which are flagged to be turned into a GUI
+    """
+    funcs = []
+    if inspect.ismodule(component):
+        members = inspect.getmembers(component)
+    elif inspect.isclass(component):
+        raise TypeError(f'Expected module or instance, got class: {component.__name__} instead')
+    elif type(component) != component.__class__:
+        raise TypeError(f'Expected new-style class, got old-style class: {component.__class__.__name__} instead')
+    else:
+        members = _get_members(component)
+
+    def _get_members(m):
+        # Prefer to get class members to avoid calling properties
+        # I'm not certain this is the desired behavior in my case, since it will prefer default function definitions
+        # over per-instance overrides. TBD.
+        cls = type(m)
+        for name in dir(instance):
+            owner = cls if hasattr(cls, name) else m
+            yield name, getattr(owner, name)
+
+    for member in members:
+        if inspect.isroutine(member):
+            if hasattr(member, _pggui_name):
+                funcs.append(member)
+    return funcs
+
+
+    def param_is_correct_type(value, anno) -> bool:
+        """
+        Identify if the given param is of the type indicated by anno
+        :param param: The value passed in to the function
+        :param anno: inspect.getfullargspec(func).annotations['<param>']
+        :return: True if it is the right type, else False
+        """
+        if get_origin(anno) is Union:
+            anno = get_args(anno)
+        return isinstance(param, anno)
+
+
+class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        self.grid(row=0, column=0)
         self.create_widgets()
         self.bcount = 1
 
     def add_button(self, text, command):
-        btn = tk.Button(self, text=text, command=command)
+        btn = ttk.Button(self, text=text, command=command)
         btn.pack(side="top")
 
     def add_cbutton(self):
-        self.add_button(str(self.bcount) * 10, self.add_cbutton)
+        # self.add_button(str(self.bcount) * 10, self.add_cbutton)
+        self.add_button(str(self.ti.get_value()) * 10, self.add_cbutton)
         self.bcount += 1
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
+        self.hi_there = ttk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.add_cbutton
-        self.hi_there.pack(side="top")
+        self.hi_there.grid(row=0, column=0)
+        self.quit = ttk.Button(self, text="QUIT", command=self.master.destroy)
+        self.btn_run = ttk.Button(self, text='RUN', command=None)
+        self.ti = TextInputSection(self, 'blah')
+        self.ti.grid(row=1, column=0)
+        self.btn_run.grid(row=998, column=999)
+        self.quit.grid(row=999, column=999)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
 
     def say_hi(self):
         print("hi there, everyone!")
 
-root = tk.Tk()
+
+class TextInputSection(ttk.Frame):
+    def __init__(self, parent: ttk.Frame, text: str):
+        super().__init__(parent)
+        self.frame = ttk.Frame(parent)
+        self.frame.grid(padx=5, pady=5)
+        self.lbl = ttk.Label(self.frame, text=text, anchor='nw', wraplength=600)
+        self.entry = ttk.Entry(self.frame, width=80)
+        self.grid_items()
+
+    def grid_items(self):
+        self.lbl.grid(row=0, sticky='w')
+        self.entry.grid(row=1, column=0, columnspan=2, sticky='w')
+
+    def get_value(self):
+        return self.entry.get()
+
+root = Tk()
 app = Application(master=root)
 app.mainloop()
